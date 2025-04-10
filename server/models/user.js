977cbcdd3617e7534834a8e2924c0f4c79318ead
Lalit Kumar <lalitkumar962789@gmail.com>
2025-04-10 15:27:28 +0530

    Switched from bcrypt to bcryptjs for deployment compatibility

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,6 @@
 // server/models/user.js
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+const bcryptjs = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
+  this.password = await bcryptjs.hash(this.password, 10);
   next();
 });
 
