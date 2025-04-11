7f6d3dd9e1af44864b675141cf56c7e1dc636c08
Lalit Kumar <lalitkumar962789@gmail.com>
2025-04-11 15:20:13 +0530

    Fix: Combined loginUser and registerUser exports

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { loginUser } = require("../controllers/authController");
 const { loginUser, registerUser } = require("../controllers/authController");
 
 const router = express.Router();
