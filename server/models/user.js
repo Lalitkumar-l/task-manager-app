6ed5c43e839cc8b3101456fd5fa3dcf77f37fba1
Lalit Kumar <lalitkumar962789@gmail.com>
2025-04-12 12:14:45 +0530

    Separated client and server folders properly

--- server/models/user.js
+++ server/models/user.js
@@ -4,11 +4,9 @@ const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true }, // ✅ Added email field
   password: { type: String, required: true },
 });
 
-// Automatically hash password before saving
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
